4a0d5b6125debf55c3e54c41f5de429f5a6e17a3
Tobiasz Muszynski <t.muszynski93@gmail.com>
2021-04-05 18:09:59 +0100

    Update script.js
    Fiddling about with the API code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const lat = 56.68573301778158;
+const lng = -4.177401596726793;
+const end = 2021-05-01;
+
 fetch(`https://api.stormglass.io/v2/astronomy/point?lat=${lat}&lng=${lng}&end=${end}`, {
   headers: {
     'Authorization': '137a8eec-95b0-11eb-a551-0242ac130002-137a8fb4-95b0-11eb-a551-0242ac130002'
@@ -5,6 +9,6 @@ fetch(`https://api.stormglass.io/v2/astronomy/point?lat=${lat}&lng=${lng}&end=${
 }).then((response) => response.json())
 .then((jsonData) => {
     
-  // Do something with response data.
+  console.log(jsonData.data["moonFraction"]); //fetching json data
 });
 
